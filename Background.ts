48bb193383ffaedb4a9107f8f3774e111cdd00b1
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2016-10-26 17:33:40 -0300

    update

--- Background.ts
+++ Background.ts
@@ -2,11 +2,12 @@
  * Listens for the app launching then creates the window
  *
  * @see http://developer.chrome.com/apps/app.window.html
- 
+*/
+declare var chrome: any;
+
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('index.html', {
     id: 'main',
     bounds: { width: 620, height: 500 }
   });
 });
-*/
