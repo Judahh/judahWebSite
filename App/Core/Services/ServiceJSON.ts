0472c70724fc9086d2c0d1fe45c0346ac98d4345
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2016-10-28 12:33:05 -0300

    update

--- App/Core/Services/ServiceJSON.ts
+++ App/Core/Services/ServiceJSON.ts
@@ -17,13 +17,22 @@ export class ServiceJSON {
     return this.http.get(this.URL+path+".json").map(this.extractData).catch(this.handleObservableError);
   }
 
-  add(name: string, path:string): Observable<any> {
+  addObservable(name: string, path:string): Observable<any> {
     let headers = new Headers({ 'Content-Type': 'application/json' });
     let options = new RequestOptions({ headers: headers });
 
     return this.http.post(this.URL, { name }, options)
                     .map(this.extractData)
                     .catch(this.handleObservableError);
+  }
+
+  addPromise(name: string, path:string): Promise<any> {
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+
+    return this.http.post(this.URL, { name }, options)
+                    .toPromise().then(this.extractData)
+                    .catch(this.handlePromiseError);
   }
 
   private extractData(response: Response) {
