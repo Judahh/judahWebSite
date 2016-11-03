7fd17cdbbcd117c07ebce8c2452cf2a6ae175b44
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2016-11-03 16:35:38 -0300

    update

--- App/View/Body/Page/PageEducation/ComponentPageEducation.ts
+++ App/View/Body/Page/PageEducation/ComponentPageEducation.ts
@@ -1,6 +1,22 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 import { Utils } from './../../../../Core/Utils/Utils';
+import { Languages } from './../../../../Core/Languages/Languages';
+import { ModelLanguages } from './../../../../Core/Languages/ModelLanguages';
+
+import { ModelDivisorBlock } from './../../Common/DivisorBlock/ModelDivisorBlock';
+import { ModelDivisor } from './../../Common/DivisorBlock/Divisor/ModelDivisor';
+import { ModelSubDivisor } from './../../Common/DivisorBlock/Divisor/SubDivisor/ModelSubDivisor';
+
+import { ModelInformation } from './../../Common/Item/ColorEffect/Font/AnimationEffect/Information/ModelInformation';
+import { ModelAnimationEffect } from './../../Common/Item/ColorEffect/Font/AnimationEffect/ModelAnimationEffect';
+import { ModelFont } from './../../Common/Item/ColorEffect/Font/ModelFont';
+import { ModelColorEffect } from './../../Common/Item/ColorEffect/ModelColorEffect';
+import { ModelItem } from './../../Common/Item/ModelItem';
+
+import { ModelEducationInformation } from './ModelEducationInformation';
+
+import { ServiceJSON } from './../../../../Core/Services/ServiceJSON';
 
 @Component({
   moduleId: module.id,
@@ -10,21 +26,88 @@ import { Utils } from './../../../../Core/Utils/Utils';
   encapsulation: ViewEncapsulation.None
 })
 export class ComponentPageEducation implements OnInit {
-  heroSubscription:any;
+  modelEducationInformation:ModelEducationInformation;
+  modelLanguages:ModelLanguages;
 
-  page:string;
-
-  //constructor(private heroService: HeroService) { }
+  arrayModelDivisorBlock:Array<ModelDivisorBlock>;
 
   ngOnInit() {
-    //this.heroes=this.heroSubscription.getHeroes();
-      //this.heroSubscription=this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
       this.initialization();
   }
 
-  constructor() {}
+  constructor(private serviceJSON: ServiceJSON) {}
 
   initialization(){
+    this.arrayModelDivisorBlock=new Array<ModelDivisorBlock>();
+    this.modelLanguages=new ModelLanguages();
+    this.modelEducationInformation=new ModelEducationInformation();
+
+    this.getLanguageService();
+    this.getInformationService();
+    this.getArrayDivisorBlockService();
+  }
+
+  private getLanguageService(){
+    var errorMessage="";
+
+    this.serviceJSON.getObservable(Languages.currentlanguageNamePath).subscribe(
+      items => this.modelLanguages=Languages.getModelLanguages(items), error => errorMessage = <any>error);
+    
+    if(errorMessage!=""){
+      alert("Error:"+errorMessage);
+    }
+  }
+
+  private getInformationService(){
+    var errorMessage="";
+
+    this.serviceJSON.getObservable('Languages/'+Utils.getFileSelector(Utils.getFileName(__filename))).subscribe(
+      items => this.modelEducationInformation=Languages.getPageLanguage(items,this.modelLanguages), error => errorMessage = <any>error);
+    
+    if(errorMessage!=""){
+      alert("Error:"+errorMessage);
+    }
+  }
+
+  private getArrayDivisorBlockService(){
+    var errorMessage="";
+
+    this.serviceJSON.getObservable('ViewLoader/'+Utils.getFileSelector(Utils.getFileName(__filename))+'ArrayDivisorBlock').subscribe(
+      item => this.getArrayModelDivisorBlock(item), error => errorMessage = <any>error);
+    
+    if(errorMessage!=""){
+      alert("Error:"+errorMessage);
+    }
+  }
+
+  private getArrayModelDivisorBlock(arrayModelDivisorBlock:Array<ModelDivisorBlock>){
+    this.arrayModelDivisorBlock=arrayModelDivisorBlock;
+    this.arrayModelDivisorBlock[0].divisor.arraySubDivisor[0].item.colorEffect.font.animationEffect.arrayInformation.push(new ModelInformation(this.modelEducationInformation.title));
+
+    this.arrayModelDivisorBlock[1].divisor.arraySubDivisor[1].item.colorEffect.font.animationEffect.arrayInformation.push(new ModelInformation(this.modelEducationInformation.intelligenceTitle));
+    for(let index=0;index<this.modelEducationInformation.intelligenceText.length;index++){
+      this.arrayModelDivisorBlock[1].arraySubDivisor[0].item.colorEffect.font.animationEffect.arrayInformation.push(new ModelInformation(this.modelEducationInformation.intelligenceText[index]));
+    }
+
+    this.arrayModelDivisorBlock[2].divisor.arraySubDivisor[1].item.colorEffect.font.animationEffect.arrayInformation.push(new ModelInformation(this.modelEducationInformation.teamworkTitle));
+    for(let index=0;index<this.modelEducationInformation.teamworkText.length;index++){
+      this.arrayModelDivisorBlock[2].arraySubDivisor[0].item.colorEffect.font.animationEffect.arrayInformation.push(new ModelInformation(this.modelEducationInformation.teamworkText[index]));
+    }
+
+    this.arrayModelDivisorBlock[3].divisor.arraySubDivisor[1].item.colorEffect.font.animationEffect.arrayInformation.push(new ModelInformation(this.modelEducationInformation.leadershipTitle));
+    for(let index=0;index<this.modelEducationInformation.leadershipText.length;index++){
+      this.arrayModelDivisorBlock[3].arraySubDivisor[0].item.colorEffect.font.animationEffect.arrayInformation.push(new ModelInformation(this.modelEducationInformation.leadershipText[index]));
+    }
+
+    this.arrayModelDivisorBlock[4].divisor.arraySubDivisor[1].item.colorEffect.font.animationEffect.arrayInformation.push(new ModelInformation(this.modelEducationInformation.languagesCommunicationTitle));
+    for(let index=0;index<this.modelEducationInformation.languagesCommunicationText.length;index++){
+      this.arrayModelDivisorBlock[4].arraySubDivisor[0].item.colorEffect.font.animationEffect.arrayInformation.push(new ModelInformation(this.modelEducationInformation.languagesCommunicationText[index]));
+    }
+
+    this.arrayModelDivisorBlock[5].divisor.arraySubDivisor[1].item.colorEffect.font.animationEffect.arrayInformation.push(new ModelInformation(this.modelEducationInformation.flexibilityTitle));
+    for(let index=0;index<this.modelEducationInformation.flexibilityText.length;index++){
+      this.arrayModelDivisorBlock[5].arraySubDivisor[0].item.colorEffect.font.animationEffect.arrayInformation.push(new ModelInformation(this.modelEducationInformation.flexibilityText[index]));
+    }
   }
 
   ngOnDestroy() {
@@ -32,5 +115,3 @@ export class ComponentPageEducation implements OnInit {
   }
 
 }
-
-
