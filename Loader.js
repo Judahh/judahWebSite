39b3756a59e207bf975952cecef6e6c50eb4cb64
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2016-11-04 17:10:48 -0300

    update

--- Loader.js
+++ Loader.js
@@ -1,10 +1,28 @@
 document.addEventListener('mousemove', mouseTooltip, false);
 function mouseTooltip(event) {
-    var tooltip = document.querySelectorAll('.DivClassTooltip');
+    var body = document.body, html = document.documentElement;
+    var height = Math.max(html.clientHeight);
+    var tooltip = document.querySelectorAll('.tooltip');
     for (var i = 0; i < tooltip.length; i++) {
         tooltip[i].style.left = event.pageX + 'px';
-        tooltip[i].style.top = event.pageY + 'px';
+        if (isBottom(tooltip[i])) {
+            alert(tooltip[i].style.margin);
+            tooltip[i].style.top = (event.pageY - (height - (2 * 17))) + 'px';
+        }
+        else {
+            tooltip[i].style.top = event.pageY + 'px';
+        }
     }
+}
+function isBottom(element) {
+    if (element.parentElement.className == "bot") {
+        return true;
+    }
+    if (element.parentElement == document.documentElement) {
+        //alert("nope");
+        return false;
+    }
+    return isBottom(element.parentElement);
 }
 System.import('SystemConfig.js').then(function () {
     System.import('Main');
--- Loader.ts
+++ Loader.ts
@@ -1,11 +1,31 @@
 document.addEventListener('mousemove', mouseTooltip, false);
 
 function mouseTooltip(event:any) {
-	var tooltip:any = document.querySelectorAll('.DivClassTooltip');
+		var body:any = document.body, html = document.documentElement;
+		var height:any = Math.max(html.clientHeight);
+		var tooltip:any = document.querySelectorAll('.tooltip');
     for (var i=0; i<tooltip.length; i++) {
         tooltip[i].style.left = event.pageX + 'px';
-        tooltip[i].style.top = event.pageY + 'px';
+        if(isBottom(tooltip[i])){
+        	alert(tooltip[i].style.margin);
+        	tooltip[i].style.top = (event.pageY-(height-(2*17))) + 'px';
+        }else{
+        	tooltip[i].style.top = event.pageY + 'px';
+        }
     }
+}
+
+function isBottom(element:any):any {
+	if(element.parentElement.className == "bot"){
+  	return true;
+  }
+  
+  if(element.parentElement == document.documentElement){
+  	//alert("nope");
+  	return false;
+  }
+  
+  return isBottom(element.parentElement);
 }
 
 System.import('SystemConfig.js').then(function () {
