d880d1b956a3915379d327caf04dea466b16dcda
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2016-11-14 09:51:23 -0300

    update

--- app/view/body/common/inputData/modelInputData.ts
+++ app/view/body/common/inputData/modelInputData.ts
@@ -8,24 +8,4 @@ export class ModelInputData {
     textInput:ModelTextInput;
     clickButton:ModelClickButton;
     checkButton:ModelCheckButton;
-    
-    private exists(object:any) {
-        return (!((object==null)||(object==undefined)));
-    }
-
-    isComboBox(){
-        return this.exists(this.comboBox);
-    }
-
-    isTextInput(){
-        return this.exists(this.textInput);
-    }
-
-    isClickButton(){
-        return this.exists(this.clickButton);
-    }
-
-    isCheckButton(){
-        return this.exists(this.checkButton);
-    }
 }
