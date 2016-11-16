920e38641e2ab14d4a4533696d99cc49c08809a8
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2016-11-16 17:44:20 -0300

    update

--- App/View/Body/Page/PageContact/ComponentPageContact.ts
+++ App/View/Body/Page/PageContact/ComponentPageContact.ts
@@ -31,6 +31,8 @@ export class ComponentPageContact implements OnInit {
   basicModelInformation:ModelInformation;
   basicModelBasicForm:ModelBasicForm;
 
+  arrayModelDivisorBlock:Array<ModelDivisorBlock>;
+
 
   input:string="inputIdSalaryCurrency";
   page:string;
@@ -43,15 +45,108 @@ export class ComponentPageContact implements OnInit {
       this.initialization();
   }
 
-  constructor() {}
+  constructor(private serviceJSON: ServiceJSON) {}
 
   initialization(){
+    this.arrayModelDivisorBlock=new Array<ModelDivisorBlock>();
+    this.modelLanguages=new ModelLanguages();
+    this.modelContactInformation=new ModelContactInformation();
+    this.basicModelInformation=new ModelInformation("");
+    this.basicModelBasicForm=new ModelBasicForm();
+
+    this.getHalfModelInformation();
+    this.getLanguageService();
+    this.getInformationService();
+    this.getArrayDivisorBlockService();
   }
 
   ngOnDestroy() {
     //this.heroSubscription.unsubscribe();
   }
 
+  private getHalfModelInformation(){
+    var errorMessage="";
+
+    this.serviceJSON.getObservable('viewLoader/halfInformation').subscribe(
+      item => this.basicModelInformation=item, error => errorMessage = <any>error);
+    
+    if(errorMessage!=""){
+      alert("Error:"+errorMessage);
+    }
+  }
+
+  private getLanguageService(){
+    var errorMessage="";
+
+    this.serviceJSON.getObservable(Languages.currentLanguageNamePath).subscribe(
+      items => this.modelLanguages=Languages.getModelLanguages(items), error => errorMessage = <any>error);
+    
+    if(errorMessage!=""){
+      alert("Error:"+errorMessage);
+    }
+  }
+
+  private getInformationService(){
+    var errorMessage="";
+
+    this.serviceJSON.getObservable('languages/'+Utils.getFileSelector(Utils.getFileName(__filename))).subscribe(
+      items => this.modelContactInformation=Languages.getPageLanguage(items,this.modelLanguages), error => errorMessage = <any>error);
+    
+    if(errorMessage!=""){
+      alert("Error:"+errorMessage);
+    }
+  }
+
+  private getArrayDivisorBlockService(){
+    var errorMessage="";
+
+    this.serviceJSON.getObservable('viewLoader/'+Utils.getFileSelector(Utils.getFileName(__filename))+'ArrayDivisorBlock').subscribe(
+      item => this.getArrayModelDivisorBlock(item), error => errorMessage = <any>error);
+    
+    if(errorMessage!=""){
+      alert("Error:"+errorMessage);
+    }
+  }
+
+  private getArrayModelDivisorBlock(arrayModelDivisorBlock:Array<ModelDivisorBlock>){
+    this.arrayModelDivisorBlock=arrayModelDivisorBlock;
+    this.arrayModelDivisorBlock[0].divisor.arraySubDivisor[0].item.colorEffect.font.animationEffect.arrayInformation.push(new ModelInformation(this.modelContactInformation.title));
+
+    this.arrayModelDivisorBlock[1].divisor.arraySubDivisor[1].item.colorEffect.font.animationEffect.arrayInformation.push(new ModelInformation(this.modelContactInformation.diplomas_CertificatesTitle));
+    for(let index=0;index<this.modelContactInformation.diplomas_CertificatesText.length;index++){
+      var modelInformation:ModelInformation=Object.create(this.basicModelInformation);
+      modelInformation.information=this.modelContactInformation.diplomas_CertificatesText[index];
+      this.arrayModelDivisorBlock[1].arraySubDivisor[0].item.colorEffect.font.animationEffect.arrayInformation.push(modelInformation);
+    }
+
+    this.arrayModelDivisorBlock[2].divisor.arraySubDivisor[1].item.colorEffect.font.animationEffect.arrayInformation.push(new ModelInformation(this.modelContactInformation.hardwareTitle));
+    for(let index=0;index<this.modelContactInformation.hardwareText.length;index++){
+      var modelInformation:ModelInformation=Object.create(this.basicModelInformation);
+      modelInformation.information=this.modelContactInformation.hardwareText[index];
+      this.arrayModelDivisorBlock[2].arraySubDivisor[0].item.colorEffect.font.animationEffect.arrayInformation.push(modelInformation);
+    }
+
+    this.arrayModelDivisorBlock[3].divisor.arraySubDivisor[1].item.colorEffect.font.animationEffect.arrayInformation.push(new ModelInformation(this.modelContactInformation.softwareTitle));
+    for(let index=0;index<this.modelContactInformation.softwareText.length;index++){
+      var modelInformation:ModelInformation=Object.create(this.basicModelInformation);
+      modelInformation.information=this.modelContactInformation.softwareText[index];
+      this.arrayModelDivisorBlock[3].arraySubDivisor[0].item.colorEffect.font.animationEffect.arrayInformation.push(modelInformation);
+    }
+
+    this.arrayModelDivisorBlock[4].divisor.arraySubDivisor[1].item.colorEffect.font.animationEffect.arrayInformation.push(new ModelInformation(this.modelContactInformation.webTitle));
+    for(let index=0;index<this.modelContactInformation.webText.length;index++){
+      var modelInformation:ModelInformation=Object.create(this.basicModelInformation);
+      modelInformation.information=this.modelContactInformation.webText[index];
+      this.arrayModelDivisorBlock[4].arraySubDivisor[0].item.colorEffect.font.animationEffect.arrayInformation.push(modelInformation);
+    }
+
+    this.arrayModelDivisorBlock[5].divisor.arraySubDivisor[1].item.colorEffect.font.animationEffect.arrayInformation.push(new ModelInformation(this.modelContactInformation.mobileTitle));
+    for(let index=0;index<this.modelContactInformation.mobileText.length;index++){
+      var modelInformation:ModelInformation=Object.create(this.basicModelInformation);
+      modelInformation.information=this.modelContactInformation.mobileText[index];
+      this.arrayModelDivisorBlock[5].arraySubDivisor[0].item.colorEffect.font.animationEffect.arrayInformation.push(modelInformation);
+    }
+  }
 }
 
 
