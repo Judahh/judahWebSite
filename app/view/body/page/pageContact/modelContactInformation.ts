8e26a39d2a0d4f6a0ed9d53426e70d182fd2b4e4
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2016-11-16 18:00:33 -0300

    update

--- app/view/body/page/pageContact/modelContactInformation.ts
+++ app/view/body/page/pageContact/modelContactInformation.ts
@@ -1,4 +1,41 @@
 import { ModelBasicForm } from './../../common/basicForm/ModelBasicForm';
 export class ModelContactInformation {
-    basicForm:ModelBasicForm;
+    id: number;
+    language:string;
+    title:string;
+    doYouWantToHireMe:string;
+    name:string;
+    company:string;
+    email:string;
+    phone:string;
+    mobile:string;
+    landline:string;
+    address:string;
+    jobTitle:string;
+    regular:string;
+    consultant:string;
+    temporary:string;
+    salary:string;
+    currencyUS:string;
+    currencyRS:string;
+    perHour:string;
+    perMonth:string;
+    perYear:string;
+    hour:string;
+    flexible:string;
+    workingHours:string;
+    in:string;
+    out:string;
+    sunday:string;
+    monday:string;
+    tuesday:string;
+    wednesday:string;
+    thursday:string;
+    friday:string;
+    saturday:string;
+    description:string;
+    curriculumVitae:string;
+    socialNetworks:string;
+    submit:string;
+    download:string
 }
