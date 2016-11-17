1d13c2e0010a7265347bbf04587ff94584bf3b63
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2016-11-17 17:08:30 -0300

    update

--- App/View/Body/Page/PageContact/ComponentPageContact.ts
+++ App/View/Body/Page/PageContact/ComponentPageContact.ts
@@ -133,6 +133,52 @@ export class ComponentPageContact implements OnInit {
     this.arrayModelDivisorBlock0=arrayModelDivisorBlock;
     this.arrayModelDivisorBlock0[0].divisor.arraySubDivisor[0].item.colorEffect.font.animationEffect.arrayInformation.push(new ModelInformation(this.modelContactInformation.title));
     this.arrayModelDivisorBlock0[1].divisor.arraySubDivisor[0].item.colorEffect.font.animationEffect.arrayInformation.push(new ModelInformation(this.modelContactInformation.doYouWantToHireMe));
+
+    this.arrayModelDivisorBlock0[1].arraySubDivisor[0].basicForm.array3InputData[0][0][0].textInput.placeholder=this.modelContactInformation.name;
+    this.arrayModelDivisorBlock0[1].arraySubDivisor[0].basicForm.array3InputData[0][1][0].textInput.placeholder=this.modelContactInformation.company;
+
+    this.arrayModelDivisorBlock0[1].arraySubDivisor[0].basicForm.array3InputData[1][0][0].textInput.placeholder=this.modelContactInformation.email;
+    this.arrayModelDivisorBlock0[1].arraySubDivisor[0].basicForm.array3InputData[1][1][0].textInput.placeholder=this.modelContactInformation.phone;
+    this.arrayModelDivisorBlock0[1].arraySubDivisor[0].basicForm.array3InputData[1][1][1].comboBox.arrayOptions[0]=this.modelContactInformation.mobile;
+    this.arrayModelDivisorBlock0[1].arraySubDivisor[0].basicForm.array3InputData[1][1][1].comboBox.arrayOptions[1]=this.modelContactInformation.landline;
+
+    this.arrayModelDivisorBlock0[1].arraySubDivisor[0].basicForm.array3InputData[2][0][0].textInput.placeholder=this.modelContactInformation.address;
+
+    this.arrayModelDivisorBlock0[1].arraySubDivisor[0].basicForm.array3InputData[3][0][0].textInput.placeholder=this.modelContactInformation.jobTitle;
+    this.arrayModelDivisorBlock0[1].arraySubDivisor[0].basicForm.array3InputData[3][0][1].comboBox.arrayOptions[0]=this.modelContactInformation.regular;
+    this.arrayModelDivisorBlock0[1].arraySubDivisor[0].basicForm.array3InputData[3][0][1].comboBox.arrayOptions[1]=this.modelContactInformation.consultant;
+    this.arrayModelDivisorBlock0[1].arraySubDivisor[0].basicForm.array3InputData[3][0][1].comboBox.arrayOptions[2]=this.modelContactInformation.temporary;
+    this.arrayModelDivisorBlock0[1].arraySubDivisor[0].basicForm.array3InputData[3][1][0].textInput.placeholder=this.modelContactInformation.currency;
+    this.arrayModelDivisorBlock0[1].arraySubDivisor[0].basicForm.array3InputData[3][1][0].textInput.arrayOptions[0]=this.modelContactInformation.currencyUS;
+    this.arrayModelDivisorBlock0[1].arraySubDivisor[0].basicForm.array3InputData[3][1][0].textInput.arrayOptions[1]=this.modelContactInformation.currencyRS;
+    this.arrayModelDivisorBlock0[1].arraySubDivisor[0].basicForm.array3InputData[3][1][1].textInput.placeholder=this.modelContactInformation.salary;
+    this.arrayModelDivisorBlock0[1].arraySubDivisor[0].basicForm.array3InputData[3][1][2].comboBox.arrayOptions[0]=this.modelContactInformation.perHour;
+    this.arrayModelDivisorBlock0[1].arraySubDivisor[0].basicForm.array3InputData[3][1][2].comboBox.arrayOptions[1]=this.modelContactInformation.perMonth;
+    this.arrayModelDivisorBlock0[1].arraySubDivisor[0].basicForm.array3InputData[3][1][2].comboBox.arrayOptions[2]=this.modelContactInformation.perYear;
+    this.arrayModelDivisorBlock0[1].arraySubDivisor[0].basicForm.array3InputData[3][1][3].checkButton.item.colorEffect.font.animationEffect.arrayInformation.push(new ModelInformation(this.modelContactInformation.flexible));
+
+    this.arrayModelDivisorBlock0[1].arraySubDivisor[0].basicForm.array3InputData[4][0][0].item.colorEffect.font.animationEffect.arrayInformation.push(new ModelInformation(this.modelContactInformation.workingHours));
+
+    this.arrayModelDivisorBlock0[1].arraySubDivisor[0].basicForm.array3InputData[5][0][0].checkButton.item.colorEffect.font.animationEffect.arrayInformation.push(new ModelInformation(this.modelContactInformation.sunday));
+    this.arrayModelDivisorBlock0[1].arraySubDivisor[0].basicForm.array3InputData[5][0][1].checkButton.item.colorEffect.font.animationEffect.arrayInformation.push(new ModelInformation(this.modelContactInformation.monday));
+    this.arrayModelDivisorBlock0[1].arraySubDivisor[0].basicForm.array3InputData[5][0][2].checkButton.item.colorEffect.font.animationEffect.arrayInformation.push(new ModelInformation(this.modelContactInformation.tuesday));
+    this.arrayModelDivisorBlock0[1].arraySubDivisor[0].basicForm.array3InputData[5][0][3].checkButton.item.colorEffect.font.animationEffect.arrayInformation.push(new ModelInformation(this.modelContactInformation.wednesday));
+    this.arrayModelDivisorBlock0[1].arraySubDivisor[0].basicForm.array3InputData[5][0][4].checkButton.item.colorEffect.font.animationEffect.arrayInformation.push(new ModelInformation(this.modelContactInformation.thursday));
+    this.arrayModelDivisorBlock0[1].arraySubDivisor[0].basicForm.array3InputData[5][0][5].checkButton.item.colorEffect.font.animationEffect.arrayInformation.push(new ModelInformation(this.modelContactInformation.friday));
+    this.arrayModelDivisorBlock0[1].arraySubDivisor[0].basicForm.array3InputData[5][0][6].checkButton.item.colorEffect.font.animationEffect.arrayInformation.push(new ModelInformation(this.modelContactInformation.saturday));
+    this.arrayModelDivisorBlock0[1].arraySubDivisor[0].basicForm.array3InputData[5][0][7].checkButton.item.colorEffect.font.animationEffect.arrayInformation.push(new ModelInformation(this.modelContactInformation.flexible));
+
+    this.arrayModelDivisorBlock0[1].arraySubDivisor[0].basicForm.array3InputData[6][0][0].textInput.placeholder=this.modelContactInformation.in;
+    this.arrayModelDivisorBlock0[1].arraySubDivisor[0].basicForm.array3InputData[6][0][1].item.colorEffect.font.animationEffect.arrayInformation.push(new ModelInformation(this.modelContactInformation.hour));
+    this.arrayModelDivisorBlock0[1].arraySubDivisor[0].basicForm.array3InputData[6][0][2].checkButton.item.colorEffect.font.animationEffect.arrayInformation.push(new ModelInformation(this.modelContactInformation.flexible));
+
+    this.arrayModelDivisorBlock0[1].arraySubDivisor[0].basicForm.array3InputData[7][0][0].textInput.placeholder=this.modelContactInformation.out;
+    this.arrayModelDivisorBlock0[1].arraySubDivisor[0].basicForm.array3InputData[7][0][1].item.colorEffect.font.animationEffect.arrayInformation.push(new ModelInformation(this.modelContactInformation.hour));
+    this.arrayModelDivisorBlock0[1].arraySubDivisor[0].basicForm.array3InputData[7][0][2].checkButton.item.colorEffect.font.animationEffect.arrayInformation.push(new ModelInformation(this.modelContactInformation.flexible));
+    
+    this.arrayModelDivisorBlock0[1].arraySubDivisor[0].basicForm.array3InputData[8][0][0].textInput.placeholder=this.modelContactInformation.description;
+
+    this.arrayModelDivisorBlock0[1].arraySubDivisor[0].basicForm.array3InputData[9][0][0].clickButton.item.colorEffect.font.animationEffect.arrayInformation.push(new ModelInformation(this.modelContactInformation.submit));
   }
 
   private getArrayModelDivisorBlock1(arrayModelDivisorBlock:Array<ModelDivisorBlock>){
