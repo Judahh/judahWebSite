13893544fcda40b3cf7adf2958aa662c3ce421b0
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2016-12-12 08:44:37 -0300

    update

--- app/view/body/common/basicTwoWay/modelBasicTwoWay.ts
+++
@@ -1,8 +0,0 @@
-import { ModelInputData } from './../inputData/ModelInputData';
-
-export class ModelBasicTwoWay {
-    arrayInputData:Array<ModelInputData>;
-    link:string;
-    onInsert:any;
-    onRemove:any;
-}
