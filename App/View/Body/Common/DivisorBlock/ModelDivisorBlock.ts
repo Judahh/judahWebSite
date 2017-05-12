3384afc714d62332e87dca6e9494d6dfdad88ed4
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2017-05-12 12:55:35 -0300

    update

--- App/View/Body/Common/DivisorBlock/ModelDivisorBlock.ts
+++
@@ -1,7 +0,0 @@
-import { ModelSubDivisor } from './divisor/subDivisor/ModelSubDivisor';
-import { ModelDivisor } from './divisor/ModelDivisor';
-
-export class ModelDivisorBlock {
-    arraySubDivisor: Array<ModelSubDivisor>;
-    divisor: ModelDivisor;
-}
