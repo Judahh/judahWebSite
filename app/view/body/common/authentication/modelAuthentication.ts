e0c484c3c6d381cced2eaa3540aaba8717405282
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2017-05-12 13:41:42 -0300

    up

--- app/view/body/common/authentication/modelAuthentication.ts
+++
@@ -1,6 +0,0 @@
-import { ModelItem } from './../item/ModelItem';
-import { ModelInputData } from './../inputData/ModelInputData';
-
-export class ModelAuthentication {
-    arrayInputData:Array<ModelInputData>;
-}
