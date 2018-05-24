a386f5006248713b6ebd83902f1ff2945832faad
Judahh <judahholanda7@gmail.com>
2018-05-24 16:00:55 -0300

    update

--- api/hardwareHandler/hardwareHandler.ts
+++ api/hardwareHandler/hardwareHandler.ts
@@ -1,9 +1,19 @@
+import { ExternalHandler } from '../externalHandler/externalHandler';
 import { BasicHardwareHandler } from 'backapijh';
 // let packageJson = require('./../package.json');
 
 export class HardwareHandler extends BasicHardwareHandler {
+    private externalHandler: ExternalHandler;
 
     constructor() {
         super();
+    }
+
+    public getExternalHandler() {
+        return this.externalHandler;
+    }
+
+    public setExternalHandler(externalHandler: ExternalHandler) {
+        this.externalHandler = externalHandler;
     }
 }
