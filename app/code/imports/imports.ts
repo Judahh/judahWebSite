40fb661919b983e716cc20594cee5b8109fd71cf
Judahh <judahholanda7@gmail.com>
2018-05-24 11:40:22 -0300

    update

--- app/code/imports/imports.ts
+++ app/code/imports/imports.ts
@@ -1,12 +1,18 @@
-import { Socket } from './../socket/socket';
+
 import { Languages } from './../languages/Languages';
 import { SetLanguage } from './../languages/setLanguage';
 import { Age } from './../age/age';
-import * as OnLoad from './../onLoad/onLoad'
+import * as loader from './../onLoad/loader';
+// tslint:disable-next-line:no-empty
+try { require('./../../style/app.css'); } catch (e) { console.log('ERROR FONT'); };
+
+let w: any = window;
+w.FontAwesomeConfig = {
+    searchPseudoElements: true
+}
+
 export {
-    Socket,
-    OnLoad,
+    loader,
     Age,
     Languages, SetLanguage
 };
-// require('./../../../node_modules/backappjh/dist/backApp.js');
